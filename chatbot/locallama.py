0132f193a9af6ad5d8e144c353dafe455f87337c
piyushprashantkopte <piyushkopte23@gmail.com>
2025-04-24 12:26:01 +0530

    change model

--- chatbot/locallama.py
+++ chatbot/locallama.py
@@ -23,11 +23,11 @@ prompt = ChatPromptTemplate.from_messages(
 
 ## streamlit framework
 
-st.title('Langchain Demo With LLAMA3.2')
+st.title('Langchain Demo With LLAMA2')
 input_text=st.text_input("Search the topic u want")
 
 # ollama llama2 LLm 
-llm=Ollama(model="llama3.2")
+llm=Ollama(model="llama2")
 output_parser=StrOutputParser()
 chain=prompt|llm|output_parser
 
